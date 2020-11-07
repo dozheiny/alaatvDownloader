a86727e16c15abb56b118cd11b181b0191077a94
dozheiny <54172214+dozheiny@users.noreply.github.com>
2020-11-07 22:01:34 +0330

    Add files via upload
    get some new links

--- test.py
+++ test.py
@@ -4,9 +4,13 @@ import requests
 url = 'https://alaatv.com/set/963'
 req = requests.get(url).text
 
-soup = bs(req, 'html.parser')
-#s = soup.find('script', type='application/ld+json')
+soup = bs(req, 'lxml')
+tags = soup.find_all('script')
+s = soup.find('script', type='application/ld+json')
+
 
 for link in soup.find_all('script', type='application/ld+json'):
 
-	print(link)
+	if 'https://' in str(link):
+		print(link)
+
